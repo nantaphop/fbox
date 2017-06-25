6e1fe854b0ff48dff5bdb4c0b341a5848f28eaee
Nantaphop <nantaphop.p@gmail.com>
2017-06-25 09:02:14 +0700

    Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,113 @@
-# fbox
-Flexbox Component for Reactjs. Powered by Styled-Components
+# FBox
+Flexbox Component for Reactjs. Powered by Styled-Components.
+
+# Motivation
+Flexbox is really powerful but it have to write so many css style. Moreover, we have to write a lot of boilerplate code to set the layout of our component for example FBox will turn following boilerplate style to something more sugar! 
+```
+display: flex;
+flex-direction: row;
+justify-content: center;
+alignItems: center;
+padding: 10px;
+```
+
+with FBox you can just write...
+
+```
+<FBox center alignCenter p="10px">.....</FBox>
+```
+
+# Installation
+```
+npm install --save fbox
+```
+# Usage
+```
+import FBox from 'fbox'
+...
+...
+<FBox alignCenter center p="10px">
+    ....Your contents...
+</FBox>
+```
+Noted: Usually, you will have to use nested FBox together to create a layout.
+
+# Example
+
+please check `/stories/index.js` for example usage. You also use `npm run storybook` to run the sample on your machine.
+
+# Props
+
+Props that is Boolean type you just have to add props name to FBox like this `<FBox center alignCenter>...</FBox>`
+
+Props that is string type you have to add value to FBox like this `<FBox background="red" p="10px" ml="2em">...</FBox>`
+
+|Prop Type| Prop Name | Generate CSS |
+|---------|-----------|---------------|
+| Boolean | column   | flex-direction: column;
+| Boolean | columnReverse | flex-direction: column-reverse; 
+| Boolean | row |  flex-direction: row; 
+| Boolean | rowReverse |  flex-direction: row-reverse; 
+| Boolean | center |  justify-content: column-reverse; 
+| Boolean | flexStart |  justify-content: column-reverse; 
+| Boolean | flexEnd | justify-content: flex-end;
+| Boolean | baseline | justify-content: baseline;
+| Boolean | spaceBetween | justify-content: space-between;
+| Boolean | spaceAround | justify-content: space-around;
+| Boolean | contentCenter | align-content: content-center;
+| Boolean | contentFlexStart | align-content: flex-start;
+| Boolean | contentFlexEnd | align-content: flex-end;
+| Boolean | contentBaseline | align-content: baseline;
+| Boolean | contentSpaceBetween | align-content: space-between;
+| Boolean | contentSpaceAround | align-content: space-around;
+| Boolean | alignCenter | align-items: center;
+| Boolean | alignFlexStart | align-items: flex-start;
+| Boolean | alignFlexEnd | align-items: flex-end;
+| Boolean | alignBaseline | align-items: baseline;
+| Boolean | alignStretch | align-items: stretch;
+| Boolean | selfCenter | align-self: center;
+| Boolean | selfFlexStart | align-self: flex-start;
+| Boolean | selfFlexEnd | align-self: flex-end;
+| Boolean | selfBaseline | align-self: baseline;
+| Boolean | selfStretch | align-self: stretch;
+| Boolean | grow1 | flex-grow: 1;
+| Boolean | grow2 | flex-grow: 2;
+| Boolean | grow3 | flex-grow: 3;
+| Boolean | grow4 | flex-grow: 4;
+| Boolean | grow5 | flex-grow: 5;
+| Boolean | shrink1 | flex-shrink: 1;
+| Boolean | shrink2 | flex-shrink: 2;
+| Boolean | shrink3 | flex-shrink: 3;
+| Boolean | shrink4 | flex-shrink: 4;
+| Boolean | shrink5 | flex-shrink: 5;
+| Boolean | wrap | flex-wrap: wrap;
+| Boolean | nowrap | flex-wrap: nowrap;
+| Boolean | wrapReverse | flex-wrap: wrap-reverse;
+| string | background | background: ____;
+| string | width | width: ____;
+| string | height | height: ____;
+| string | p | padding: ____;
+| string | pt | padding-top: ____;
+| string | pr | padding-right: ____;
+| string | pb | padding-bottom: ____;
+| string | pl | padding-left: ____;
+| string | m | margin: ____;
+| string | mt | margin-top: ____;
+| string | mr | margin-right: ____;
+| string | mb | margin-bottom: ____;
+| string | ml | margin-left: ____;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- dist/FBox.js
+++ dist/FBox.js
@@ -4,7 +4,7 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 
-var _templateObject = _taggedTemplateLiteral(['\n    display: flex;\n'], ['\n    display: flex;\n']);
+var _templateObject = _taggedTemplateLiteral(['\n    display: flex;\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n'], ['\n    display: flex;\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n    ', '\n']);
 
 var _styledComponents = require('styled-components');
 
@@ -14,6 +14,33 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _taggedTemplateLiteral(strings, raw) { return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
 
-var FBox = _styledComponents2.default.div(_templateObject);
+var toAttr = function toAttr(attr) {
+    return function (propsName, attrValue) {
+        return function (props) {
+            return props[propsName] && attr + ': ' + (attrValue ? attrValue : propsName) + ';';
+        };
+    };
+};
+var useAttrIfPropsExist = function useAttrIfPropsExist(propsName) {
+    return function (props) {
+        return props[propsName] && propsName + ': ' + props[propsName] + ';';
+    };
+};
+var useAttrIfProxyPropsExist = function useAttrIfProxyPropsExist(propsName, attr) {
+    return function (props) {
+        return props[propsName] && attr + ': ' + props[propsName] + ';';
+    };
+};
+
+var flexDirection = toAttr('flex-direction');
+var justifyContent = toAttr('justify-content');
+var alignItems = toAttr('align-items');
+var alignSelf = toAttr('align-self');
+var alignContent = toAttr('align-content');
+var flexGrow = toAttr('flex-grow');
+var flexShrink = toAttr('flex-shrink');
+var flexWrap = toAttr('flex-wrap');
+
+var FBox = _styledComponents2.default.div(_templateObject, flexDirection('column'), flexDirection('columnReverse', 'column-reverse'), flexDirection('row'), flexDirection('rowReverse', 'row-reverse'), justifyContent('center'), justifyContent('flexStart', 'flex-start'), justifyContent('flexEnd', 'flex-end'), justifyContent('baseline'), justifyContent('spaceBetween', 'space-between'), justifyContent('spaceAround', 'space-around'), alignContent('contentCenter', 'center'), alignContent('contentFlexStart', 'flex-start'), alignContent('contentFlexEnd', 'flex-end'), alignContent('contentBaseline', 'baseline'), alignContent('contentSpaceBetween', 'space-between'), alignContent('contentSpaceAround', 'space-around'), alignItems('alignCenter', 'center'), alignItems('alignFlexStart', 'flex-start'), alignItems('alignFlexEnd', 'flex-end'), alignItems('alignBaseline', 'baseline'), alignItems('alignStretch', 'stretch'), alignSelf('selfCenter', 'center'), alignSelf('selfFlexStart', 'flex-start'), alignSelf('selfFlexEnd', 'flex-end'), alignSelf('selfBaseline', 'baseline'), alignSelf('selfStretch', 'stretch'), flexGrow('grow1', '1'), flexGrow('grow2', '2'), flexGrow('grow3', '3'), flexGrow('grow4', '4'), flexGrow('grow5', '5'), flexGrow('shrink1', '1'), flexGrow('shrink2', '2'), flexGrow('shrink3', '3'), flexGrow('shrink4', '4'), flexGrow('shrink5', '5'), flexWrap('wrap'), flexWrap('nowrap'), flexWrap('wrapReverse', 'wrap-reverse'), useAttrIfPropsExist('background'), useAttrIfPropsExist('width'), useAttrIfPropsExist('height'), useAttrIfProxyPropsExist('p', 'padding'), useAttrIfProxyPropsExist('pt', 'padding-top'), useAttrIfProxyPropsExist('pr', 'padding-right'), useAttrIfProxyPropsExist('pb', 'padding-bottom'), useAttrIfProxyPropsExist('pl', 'padding-left'), useAttrIfProxyPropsExist('m', 'margin'), useAttrIfProxyPropsExist('mt', 'margin-top'), useAttrIfProxyPropsExist('mr', 'margin-right'), useAttrIfProxyPropsExist('mb', 'margin-bottom'), useAttrIfProxyPropsExist('ml', 'margin-left'));
 
 exports.default = FBox;
